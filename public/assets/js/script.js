b37f2d70051f5a7d04d4e4fafbf2794d647bc126
alex <fredwolk6@gmail.com>
2023-01-03 01:07:32 +0300

    fix

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,11 +3,11 @@ $('.header--search-btn').on('click', function () {
     $('.header--search-input').toggleClass('active');
 });
 
-// $(document).click(function (e) {
-//     if (!$(".header--search-input-wrapper").is(e.target) && $(".header--search-input-wrapper").has(e.target).length === 0) {
-//         $('.header--search-btn, .header--search-input').removeClass('active');
-//     }
-// });
+$(document).click(function (e) {
+    if (!$(".header--search-input-wrapper").is(e.target) && $(".header--search-input-wrapper").has(e.target).length === 0) {
+        $('.header--search-btn, .header--search-input').removeClass('active');
+    }
+});
 
 $('.btn--burger').on('click', function () {
     $(this).toggleClass('active');
