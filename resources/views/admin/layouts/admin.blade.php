bf1bdd593fc2b192372c1bb37a3c025e943a8233
fred <fredwolk6@gmail.com>
2023-01-27 16:33:13 +0300

    article fix

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -7,6 +7,7 @@
 
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ asset('assets/admin/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/admin/css/adminlte.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/admin/plugins/summernote/summernote-bs4.min.css') }}">
