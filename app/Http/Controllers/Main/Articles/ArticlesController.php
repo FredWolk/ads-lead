3f229b91821d9b917935dd952c10e69d6f9b67ab
fred <fredwolk6@gmail.com>
2023-01-28 14:15:46 +0300

    article fix

--- app/Http/Controllers/Main/Articles/ArticlesController.php
+++ app/Http/Controllers/Main/Articles/ArticlesController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Main\Articles;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Seo;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
 class ArticlesController extends Controller
@@ -14,9 +13,10 @@ class ArticlesController extends Controller
     {
         $locale = App::getLocale() == 'en' ? '' : 'pt_';
         $seo = Seo::where('page', Seo::ARTICLES_PAGE)->first();
-        if (!empty($seo))
+        if (!empty($seo)) {
             $seo->toArray();
-        $articles = Article::with('author')->where('type', 'article')->paginate(9);
+        }
+        $articles = Article::with('author')->where('type', 'article')->orderByDesc('id')->paginate(9);
         return view('main.articles.all-articles', compact('articles', 'seo', 'locale'));
     }
 }
