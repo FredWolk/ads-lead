47ee5f4311d4cc699eddf0a1a439ae49072e74cc
fred <fredwolk6@gmail.com>
2023-02-01 21:56:03 +0300

    translate file

--- lang/pt/messages.php
+++ lang/pt/messages.php
@@ -1,6 +1,81 @@
 <?php
 
 return [
-    'index' => 'Esta é a página de índice',
-    'title' => 'pt article',
+    'articles' => 'Artigos',
+    'video' => 'Vídeo',
+    'cpa' => 'Fórum',
+    'ad' => 'Redes de Afiliados',
+    'forum' => 'Redes de Propaganda',
+    'services' => 'Serviços',
+
+    'lasted' => 'Últimos Artigos',
+    'show' => 'Mostrar Mais',
+    'over' => 'Visão Geral',
+    'youtube' => 'Nosso YouTube',
+    'activities' => 'Eventos',
+    'calendar' => 'Calendário',
+    'contact' => 'Contactos',
+    'base' => 'Banco de Conhecimentos',
+    'subscribe' => 'Subscrever',
+    'policy' => 'Política de Privacidade',
+
+    'vertical' => 'Orientação',
+    'countries' => 'Países',
+    'payment_models' => 'Modelos de pagamento',
+    'regularity_payments' => 'Periodicidade de pagamentos',
+    'payment_systems' => 'Sistemas de Pagamento',
+
+    'minimum_amount' => 'Depósito Mínimo',
+
+    'ad_types' => 'Formatos de Propaganda',
+    'promocode' => 'Código de Promoção',
+    'сontent' => 'Sumário',
+    'who' => 'Destinatários',
+    'share' => 'Compartilhar',
+
+    'other.articles' => 'Ouros Artigos',
+    'other.videos' => 'Outros vídeos',
+    'other.cpa' => 'Odinião do Editor',
+    'other.ad' => 'Outras redes de propaganda',
+    'other.events' => 'Outros eventos',
+
+    'comments' => 'Comentários',
+    'comments.text' => 'Comentar vídeo no nosso canal de YouTube',
+
+    'сhoose' => 'Selecionar o mês',
+
+    'rating' => 'Classificação',
+    'support' => 'Suporte',
+    'payouts' => 'Pagamentos',
+    'offers' => 'Ofertas',
+    'rates' => 'Rate/Preços',
+
+    'cpa.year' => 'Ano de fundação',
+    'cpa.gep' => 'Geografia de ofertas',
+    'cpa.verticals' => 'Orientação',
+    'cpa.inhouse' => 'Ofertas In-house',
+    'cpa.minimum' => 'Saque mínimo',
+    'cpa.payment' => 'Sistemas de Pagamento',
+    'cpa.payment.method' => 'Periodicidade de Pagamentos',
+    'cpa.affiliate' => 'Programa de Referências',
+    'cpa.tools' => 'Ferramentas',
+
+    'opinion' => 'Odinião do Editor',
+
+    'ad.year' => 'Ano de fundação',
+    'ad.month' => 'Tráfego mensal',
+    'ad.formats' => 'Formatos',
+    'ad.banned' => 'Temas proibídas',
+    'ad.minimum' => 'Preço mínimo por click',
+    'ad.payment' => 'Ferramentas de Pagamento',
+    'ad.minimal' => 'Depósito Mínimo',
+    'ad.affiliate' => 'Programa de Referências',
+    'ad.tools' => 'Ferramentas',
+
+    'quality' => 'Qualidade do Tráfego',
+    'geo' => 'Cobertura GEO',
+    'click' => 'Preço médio por click',
+
+    'index.h1' => 'ou estás com tráfego, ou és tráfego',
+    'index.events' => 'Próximo Evento',
 ];
