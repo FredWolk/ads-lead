101be263c9aa1c24a9941710cda29cfdf373d26a
fred <fredwolk6@gmail.com>
2023-02-01 15:14:20 +0300

    add column to cpa table

--- resources/views/main/cpa/page.blade.php
+++ resources/views/main/cpa/page.blade.php
@@ -199,7 +199,8 @@
                             @endif
                         </ul>
                         <div class="cpaartpage_card_bot">
-                            <div class="articlepage--review-integration_autor">
+                            <a href="{{ $cpa['manager_link'] }}" target="_blank"
+                               class="articlepage--review-integration_autor">
                                 <div class="videopage_main-underimage_autor-image">
                                     <img src="{{asset('storage/' . $cpa['manager_image'])}}" alt="autor">
                                 </div>
@@ -207,7 +208,7 @@
                                     <p class="articlepage--review-integration_autor-text-wrapp-name">{{ $cpa['manager_name'] }}</p>
                                     <p class="articlepage--review-integration_autor-text-wrapp-company">{{ $cpa['manager_position'] }}</p>
                                 </div>
-                            </div>
+                            </a>
                             <div class="cpaartpage_card_bot--line"></div>
                             <div class="cpaartpage_card_bot_social">
                                 @if(!empty($cpa['facebook']))
@@ -301,7 +302,7 @@
                                             Opinion</p>
                                     </div>
                                 </div>
-                                <p class="articlepage--comments-none-editorial-opinion-text">{{ $cpa['editorial_opinion'] }}</p>
+                                <p class="articlepage--comments-none-editorial-opinion-text">{{ $cpa["{$locale}editorial_opinion"] }}</p>
                                 {{--                        TODO: доделать после личного кабинета                           --}}
                                 @if(false)
                                     <ul class="articlepage--comments_main_list">
@@ -586,7 +587,9 @@
                                         @endfor
                                     </div>
                                 </div>
-                                <div class="cpaartpage_aside_rating_group-item-num"><span>{{ $cpa['rating_support'] }}</span>/5</div>
+                                <div class="cpaartpage_aside_rating_group-item-num">
+                                    <span>{{ $cpa['rating_support'] }}</span>/5
+                                </div>
                             </div>
                             <div class="cpaartpage_aside_rating_group-item">
                                 <div class="articlepage--comments_main_list-item_top_rating-group-item">
@@ -603,7 +606,9 @@
                                         @endfor
                                     </div>
                                 </div>
-                                <div class="cpaartpage_aside_rating_group-item-num"><span>{{ $cpa['rating_payments'] }}</span>/5</div>
+                                <div class="cpaartpage_aside_rating_group-item-num">
+                                    <span>{{ $cpa['rating_payments'] }}</span>/5
+                                </div>
                             </div>
                             <div class="cpaartpage_aside_rating_group-item">
                                 <div class="articlepage--comments_main_list-item_top_rating-group-item">
@@ -620,7 +625,9 @@
                                         @endfor
                                     </div>
                                 </div>
-                                <div class="cpaartpage_aside_rating_group-item-num"><span>{{ $cpa['rating_offers'] }}</span>/5</div>
+                                <div class="cpaartpage_aside_rating_group-item-num">
+                                    <span>{{ $cpa['rating_offers'] }}</span>/5
+                                </div>
                             </div>
                             <div class="cpaartpage_aside_rating_group-item">
                                 <div class="articlepage--comments_main_list-item_top_rating-group-item">
@@ -637,7 +644,9 @@
                                         @endfor
                                     </div>
                                 </div>
-                                <div class="cpaartpage_aside_rating_group-item-num"><span>{{ $cpa['rating_betting'] }}</span>/5</div>
+                                <div class="cpaartpage_aside_rating_group-item-num">
+                                    <span>{{ $cpa['rating_betting'] }}</span>/5
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -648,7 +657,8 @@
                                     @if($banner->type == 'image')
                                         <img width="100%" src="{{asset('storage/'. $banner->file)}}" alt="bunner">
                                     @else
-                                        <video width="100%" src="{{ asset('storage/'. $banner->file) }}" autoplay loop muted></video>
+                                        <video width="100%" src="{{ asset('storage/'. $banner->file) }}" autoplay loop
+                                               muted></video>
                                     @endif
                                 </a>
                             </div>
@@ -666,7 +676,8 @@
                 <ul class="swiper-wrapper">
                     @foreach ($moreCpa as $i)
                         <li class="article--card swiper-slide">
-                            <a class="article--card-link" href="{{ route('cpa.page', [$i['main_verticales'], $i['link']]) }}"></a>
+                            <a class="article--card-link"
+                               href="{{ route('cpa.page', [$i['main_verticales'], $i['link']]) }}"></a>
                             <img src="{{ asset('storage/'. $i["{$locale}image"]) }}" alt="banner">
                             <div class="article--card_info">
                                 <ul class="article--card_info_tags-list">
@@ -682,7 +693,12 @@
                                 <h3 class="article--card_info-title">{{ $i["{$locale}name"] }}</h3>
 
                                 <div class="article--card_info-views">
-                                    <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.99967 1.29167C8.07227 1.2881 9.1241 1.58729 10.0342 2.15484C10.9444 2.72239 11.6759 3.53526 12.1447 4.5C11.6765 5.46517 10.9451 6.27843 10.0348 6.84606C9.12453 7.41368 8.07242 7.71259 6.99967 7.70833C5.92693 7.71259 4.87481 7.41368 3.96454 6.84606C3.05427 6.27843 2.3229 5.46517 1.85467 4.5C2.32345 3.53526 3.05496 2.72239 3.9651 2.15484C4.87525 1.58729 5.92708 1.2881 6.99967 1.29167V1.29167ZM6.99967 0.125C4.08301 0.125 1.59217 1.93917 0.583008 4.5C1.59217 7.06083 4.08301 8.875 6.99967 8.875C9.91634 8.875 12.4072 7.06083 13.4163 4.5C12.4072 1.93917 9.91634 0.125 6.99967 0.125ZM6.99967 3.04167C7.38645 3.04167 7.75738 3.19531 8.03087 3.4688C8.30436 3.74229 8.45801 4.11323 8.45801 4.5C8.45801 4.88677 8.30436 5.25771 8.03087 5.5312C7.75738 5.80469 7.38645 5.95833 6.99967 5.95833C6.6129 5.95833 6.24197 5.80469 5.96848 5.5312C5.69499 5.25771 5.54134 4.88677 5.54134 4.5C5.54134 4.11323 5.69499 3.74229 5.96848 3.4688C6.24197 3.19531 6.6129 3.04167 6.99967 3.04167V3.04167ZM6.99967 1.875C5.55301 1.875 4.37467 3.05333 4.37467 4.5C4.37467 5.94667 5.55301 7.125 6.99967 7.125C8.44634 7.125 9.62467 5.94667 9.62467 4.5C9.62467 3.05333 8.44634 1.875 6.99967 1.875Z" fill="#181A1C"/></svg>
+                                    <svg width="14" height="9" viewBox="0 0 14 9" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M6.99967 1.29167C8.07227 1.2881 9.1241 1.58729 10.0342 2.15484C10.9444 2.72239 11.6759 3.53526 12.1447 4.5C11.6765 5.46517 10.9451 6.27843 10.0348 6.84606C9.12453 7.41368 8.07242 7.71259 6.99967 7.70833C5.92693 7.71259 4.87481 7.41368 3.96454 6.84606C3.05427 6.27843 2.3229 5.46517 1.85467 4.5C2.32345 3.53526 3.05496 2.72239 3.9651 2.15484C4.87525 1.58729 5.92708 1.2881 6.99967 1.29167V1.29167ZM6.99967 0.125C4.08301 0.125 1.59217 1.93917 0.583008 4.5C1.59217 7.06083 4.08301 8.875 6.99967 8.875C9.91634 8.875 12.4072 7.06083 13.4163 4.5C12.4072 1.93917 9.91634 0.125 6.99967 0.125ZM6.99967 3.04167C7.38645 3.04167 7.75738 3.19531 8.03087 3.4688C8.30436 3.74229 8.45801 4.11323 8.45801 4.5C8.45801 4.88677 8.30436 5.25771 8.03087 5.5312C7.75738 5.80469 7.38645 5.95833 6.99967 5.95833C6.6129 5.95833 6.24197 5.80469 5.96848 5.5312C5.69499 5.25771 5.54134 4.88677 5.54134 4.5C5.54134 4.11323 5.69499 3.74229 5.96848 3.4688C6.24197 3.19531 6.6129 3.04167 6.99967 3.04167V3.04167ZM6.99967 1.875C5.55301 1.875 4.37467 3.05333 4.37467 4.5C4.37467 5.94667 5.55301 7.125 6.99967 7.125C8.44634 7.125 9.62467 5.94667 9.62467 4.5C9.62467 3.05333 8.44634 1.875 6.99967 1.875Z"
+                                            fill="#181A1C"/>
+                                    </svg>
                                     <span>{{ $i['views'] }}</span>
                                 </div>
                             </div>
