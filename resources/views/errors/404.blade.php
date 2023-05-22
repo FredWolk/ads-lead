25450275531ff779eae188085884ce27848c2afe
fred <fredwolk6@gmail.com>
2023-05-22 16:53:59 +0300

    seo fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -23,68 +23,8 @@
                     </a>
                 </li>
                 <li class="errorpage--item">
-                    <a class="link--black-rotateble-arrows" href="{{ route('index') }}">
-                        <span>instagram</span>
-                        <div class="link--black-rotateble-arrows-group">
-                            @for ($a=0; $a<3; $a++)
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                          d="M8.37879 4.5H3.00011V1.5H13.5001V12H10.5001V6.62132L4.06077 13.0607L1.93945 10.9393L8.37879 4.5Z"
-                                          fill="#272C31"/>
-                                </svg>
-                            @endfor
-                        </div>
-                    </a>
-                </li>
-                <li class="errorpage--item">
-                    <a class="link--black-rotateble-arrows" href="{{ route('index') }}">
+                    <a class="link--black-rotateble-arrows" href="{{ route('articles') }}">
                         <span>Articles</span>
-                        <div class="link--black-rotateble-arrows-group">
-                            @for ($a=0; $a<3; $a++)
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                          d="M8.37879 4.5H3.00011V1.5H13.5001V12H10.5001V6.62132L4.06077 13.0607L1.93945 10.9393L8.37879 4.5Z"
-                                          fill="#272C31"/>
-                                </svg>
-                            @endfor
-                        </div>
-                    </a>
-                </li>
-                <li class="errorpage--item">
-                    <a class="link--black-rotateble-arrows" href="{{ route('index') }}">
-                        <span>facebook</span>
-                        <div class="link--black-rotateble-arrows-group">
-                            @for ($a=0; $a<3; $a++)
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                          d="M8.37879 4.5H3.00011V1.5H13.5001V12H10.5001V6.62132L4.06077 13.0607L1.93945 10.9393L8.37879 4.5Z"
-                                          fill="#272C31"/>
-                                </svg>
-                            @endfor
-                        </div>
-                    </a>
-                </li>
-                <li class="errorpage--item">
-                    <a class="link--black-rotateble-arrows" href="{{ route('index') }}">
-                        <span>Forum</span>
-                        <div class="link--black-rotateble-arrows-group">
-                            @for ($a=0; $a<3; $a++)
-                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                          d="M8.37879 4.5H3.00011V1.5H13.5001V12H10.5001V6.62132L4.06077 13.0607L1.93945 10.9393L8.37879 4.5Z"
-                                          fill="#272C31"/>
-                                </svg>
-                            @endfor
-                        </div>
-                    </a>
-                </li>
-                <li class="errorpage--item">
-                    <a class="link--black-rotateble-arrows" href="{{ route('index') }}">
-                        <span>telegram</span>
                         <div class="link--black-rotateble-arrows-group">
                             @for ($a=0; $a<3; $a++)
                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
