1a7de65029bdea63827e90a8cebd9f460d8f1043
fred <fredwolk6@gmail.com>
2023-07-29 17:18:54 +0300

    Отображение Событий

--- resources/views/main/ad/networks.blade.php
+++ resources/views/main/ad/networks.blade.php
@@ -457,7 +457,8 @@
                 @endempty
                 <div class="cpapage_main-to-show_filter">
                     <div class="cpapage_main-to-show_filter_top">
-                        <p class="cpapage_main-to-show_filter-text">Filters@if(false)
+                        <p class="cpapage_main-to-show_filter-text">Filters
+                            @if(false)
                                 <span>32</span>
                             @endif</p>
                         <button type="button" class="btn-rectangle btn--filter">
