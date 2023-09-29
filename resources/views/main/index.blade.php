333306051cf8c3073f62892ad3d7e9b617f7e1b1
fred <fredwolk6@gmail.com>
2023-09-29 15:36:50 +0300

    events links

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -673,7 +673,7 @@
                                     <li class="calendar_date init">
                                         <p>{{ date('d', strtotime($k)) }}</p>
                                         <p>{{ mb_strimwidth($i["{$locale}name"], 0, 20).'...' }}</p>
-                                        <a href="{{ route('event.page', $event['link']) }}" class="popup_event-wrap">
+                                        <a href="{{ route('event.page', $i['link']) }}" class="popup_event-wrap">
                                             <div class="popup_event">
                                                 <img loading="lazy" class="popup_event-image"
                                                      src="{{ asset('storage/'. $i["{$locale}image"]) }}" alt="">
