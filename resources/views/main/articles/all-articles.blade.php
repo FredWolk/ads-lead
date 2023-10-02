87064d10c1df0cce3337a09bf0a6586f35299b6d
fred <fredwolk6@gmail.com>
2023-10-02 17:28:05 +0300

    pagination

--- resources/views/main/articles/all-articles.blade.php
+++ resources/views/main/articles/all-articles.blade.php
@@ -149,7 +149,7 @@
                 @endforeach
             </ul>
             <div class="pagination">
-                {{ $articles->onEachSide(2)->links() }}
+                {{ $articles->onEachSide(0)->links() }}
             </div>
             @if(!empty($seo) && empty($_GET['page']))
                 <aside class="articles_seo-text">
