64549cfe3e56f51aa6f79213e014ba65cfccacf4
fred <fredwolk6@gmail.com>
2023-10-08 15:47:20 +0300

    Tools in admin

--- app/Http/Controllers/Main/Services/NetworksController.php
+++ app/Http/Controllers/Main/Services/NetworksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Main\Services;
 
 use App\Http\Controllers\Controller;
 use App\Models\BannerAside;
+use App\Models\Seo;
 use App\Models\Services;
 use Illuminate\Support\Facades\App;
 
@@ -11,8 +12,7 @@ class NetworksController extends Controller
 {
     public function __invoke()
     {
-//        $seo = Seo::where('page', Seo::ADS_PAGE)->first();
-        $seo = [];
+        $seo = Seo::where('page', Seo::TOOLS_PAGE)->first();
         $banner = BannerAside::where('show', 'all')->where('status', 1)->first();
         $recomended = Services::all()->where('is_recomendated', 1)->take(2);
         $services = Services::paginate(5);
