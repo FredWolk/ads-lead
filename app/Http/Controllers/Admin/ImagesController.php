382d45f9b3fe2050c9ac4cc72b92ac5e8b092e50
alex <fredwolk6@gmail.com>
2023-10-18 13:46:11 +0300

    image fix

--- app/Http/Controllers/Admin/ImagesController.php
+++ app/Http/Controllers/Admin/ImagesController.php
@@ -11,7 +11,7 @@ class ImagesController extends Controller
 {
     public function index()
     {
-        $images = Images::all()->sortDesc('id');
+        $images = Images::all()->orderBy('id', 'desc');
         return view('admin.images.index', compact('images'));
     }
 
