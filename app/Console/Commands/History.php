1dbeace5908692efc7880c164e8a76927b3f941b
alex <fredwolk6@gmail.com>
2024-01-16 11:08:53 +0300

    add cron history

---
+++ app/Console/Commands/History.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class History extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:history';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $shows = \App\Models\ShowBanner::all();
+        if ($shows->count() > 0){
+            foreach ($shows->toArray() as $show){
+                \App\Models\HistoryShowBanners::create([
+                    'banner' => $show['banner'],
+                    'views' => $show['views'],
+                    'clicks' => $show['clicks'],
+                ]);
+            }
+            \App\Models\ShowBanner::truncate();
+        }
+        return Command::SUCCESS;
+    }
+}
